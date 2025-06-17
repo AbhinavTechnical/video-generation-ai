a5bcf120b19cb7903721e222c04c0aed44c6caca
AbhinavTechnical <Priyankaweb25@gmail.com>
2025-06-17 19:21:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ UPLOAD_FOLDER = 'static/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
